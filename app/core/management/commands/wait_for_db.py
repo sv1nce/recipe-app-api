5f4c8bf4b34ceb2818c1224abdd09303cdf274c9
Sviatoslav Kushnir <sviatoslav.kushnir.work@gmail.com>
2024-03-25 22:22:05 +0200

    Third Commit

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,7 @@
-"""
-Django command to wait for database to be available
-"""
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -8,4 +9,18 @@ class Command(BaseCommand):
     """Django command to wait for Database"""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while not db_up:
+            try:
+                # Try to access the default database
+                connection = connections['default']
+                connection.ensure_connection()
+                db_up = True
+            except OperationalError:
+                # If connection fails, wait and try again
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available'))
+
