7ba1dddb084ddedb3c7dea70fd290e64c612bee4
Sviatoslav Kushnir <sviatoslav.kushnir.work@gmail.com>
2024-03-26 17:55:33 +0200

    Fixes to DB

--- app/core/models.py
+++ app/core/models.py
@@ -18,7 +18,7 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
